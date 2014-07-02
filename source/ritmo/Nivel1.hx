367a7d0a3ffd9fdd10a504c720790b69f89e182b
FD253 <fedemie@gmail.com>
2014-07-02 14:21:08 -0300

    Agregado código de ejemplo para reproducir secuencia

--- source/ritmo/Nivel1.hx
+++ source/ritmo/Nivel1.hx
@@ -2,6 +2,8 @@ package ritmo;
 
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.ui.FlxButton;
+import flixel.util.FlxTimer;
 
 /**
  * ...
@@ -9,10 +11,39 @@ import flixel.FlxG;
  */
 class Nivel1 extends FlxState
 {
+	var _acumulador = 0;
+	// Creamos un array para grabar lo que hace el usuario
+	var _usuario : Array<Int>; // = [for (x in 0...Niveles.nivel1.length) 0];
+	
+	private function _avanceReproduccion(Timer : FlxTimer): Void
+	{
+		if (Niveles.nivel1[_acumulador] == 1)
+		{
+			FlxG.sound.play(AssetPaths.ritmo_bell__wav);
+		}
+		_acumulador += 1;
+	}
+	
+	private function _avanceGrabacion(Timer : FlxTimer): Void
+	{
+		// Definimos un array de la misma duración para grabar lo que hace el usuario
+		
+	}
+	
+	private function _testJuego(): Void
+	{
+		new FlxTimer(Niveles.duracionSlot, _avanceGrabacion, Niveles.nivel1.length);
+	}
 
 	override public function create():Void 
 	{
 		super.create();
-		FlxG.sound.play(AssetPaths.ritmo_bell__wav);
+		// Creamos un timer para que reproduzca el sonido definido para el nivel
+		new FlxTimer(Niveles.duracionSlot, _avanceReproduccion, Niveles.nivel1.length);
+		add(new FlxButton(10, 10, "Jugar", _testJuego));
 	}
+	
+	
+	
+	
 }
--- source/ritmo/Niveles.hx
+++ source/ritmo/Niveles.hx
@@ -11,5 +11,11 @@ class Niveles
 	{
 		
 	}
+	public static var duracionSlot : Float = 0.5;	// Determina la duración de un slot de tiempo en el array de la secuencia
 	
+	// La secuencia para cada nivel está dada por un array de números.
+	// Cada número representa un slot o intervalo de tiempo que tiene una duración definida en duracionSlot
+	// Un 0 indica que en ese slot hay silencio
+	// Un número distinto de 0 (Por convención usamos el 1) indica que se debe reproducir un sonido en ese momento
+	public static var nivel1 : Array<Int> = [0,0,0,0,1,0,0,0,1,1,1,1,1,0,1];
 }
