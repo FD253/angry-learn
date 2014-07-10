b5190561712b4966490e311ce0c2d91e85e9825b
ganiserb <g.gabriel.b@gmail.com>
2014-07-10 11:50:57 -0300

    Agregada estructura de lógica y nivel

--- source/karaoke/MenuNiveles.hx
+++ source/karaoke/MenuNiveles.hx
@@ -6,9 +6,17 @@ package karaoke;
  */
 class MenuNiveles extends MenuDeEnlace
 {
+	function nivel1OnClick() {
+		Logica.nivelInicio = Nivel.nivel1;
+		FlxG.switchState(new Logica());
+	}
+	
 	override public function create():Void 
 	{
 		super.create();
+		
+		botonesDeMenu.add(new FlxButton(0, 0, "1", nivel1OnClick));
+		
 		agregarBoton("VOLVER", MenuRitmoLector);
 		agregarBoton("Reiniciar");
 		ordenarBotones();
