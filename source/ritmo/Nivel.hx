c71d4f1685d0e91905a860e71f8c9f83cad71a73
ganiserb <g.gabriel.b@gmail.com>
2014-08-04 15:48:38 -0300

    Agregados más niveles

--- source/ritmo/Nivel.hx
+++ source/ritmo/Nivel.hx
@@ -8,17 +8,52 @@ import flixel.addons.editors.ogmo.FlxOgmoLoader;
 class Nivel
 {
 	public static var niveles = [	// Array<Nivel>
+		// Estructuras 1-5
 		new Nivel(
 			[1,1,1],
-			0.3
+			0.4
 		),
 		new Nivel(
-			[1,1,0,1],
-			1
+			[1,1,0,1,1],
+			0.4
 		),
+		new Nivel(
+			[1,0,1,1],
+			0.4
+		),
+		new Nivel(
+			[1,0,1,0,1],
+			0.4
+		),
+		new Nivel(
+			[1,1,1,1],
+			0.4
+		),
+		
+		// Estructuras 6-11
 		new Nivel(
 			[1,0,1,1,1],
-			1
+			0.4
+		),
+		new Nivel(
+			[1,1,0,1,0,1],
+			0.4
+		),
+		new Nivel(
+			[1,1,0,1,1,0,1,1],
+			0.4
+		),
+		new Nivel(
+			[1,1,0,1,1,1],
+			0.4
+		),
+		new Nivel(
+			[1,0,1,0,1,0,1],
+			0.4
+		),
+		new Nivel(
+			[1,0,1,1,1,1],
+			0.4
 		),
 	];
 	
