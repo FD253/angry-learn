3aaea069abcc19e6daa76ba67b6c565647015959
FD253 <fedemie@gmail.com>
2014-10-16 05:16:01 -0300

    arreglado bugs y limpiado codigo

--- source/karaoke/Logica.hx
+++ source/karaoke/Logica.hx
@@ -1,6 +1,8 @@
 package karaoke;
 
+import flixel.addons.display.FlxNestedSprite;
 import flixel.addons.ui.FlxUIPopup;
+import flixel.addons.ui.FlxUIText;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
@@ -23,17 +25,23 @@ class Logica extends FlxState
 	// STATIC ATRIBUTES
 	public static var nivelInicio : Nivel;
 	
-	// PUBLIC ATRIBUTES
-	
 	
 	// PRIVATE ATRIBUTES
 	var nivel : Nivel;
-	var posicionNivel : Int; //posicion en el array de niveles
-	var timer : FlxTimer; //avanzador por silabas
+	var posicionNivel : Int;
+	var timer : FlxTimer;
+	var item : Item;
 	
 	var textItem : FlxText;
-	var formatoTween : FlxTextFormat;
-	var contador : Int;
+	var color : FlxTextFormat; //pseudotween
+	
+
+	var bandera: Bool;
+	
+	var btnCorrecto : FlxUIButton;
+	var btnComenzar: FlxUIButton;
+	
+
 	
 	override public function create() {
 	
@@ -41,36 +49,22 @@ class Logica extends FlxState
 		nivel = nivelInicio;
 		posicionNivel = 0; //deberia ser random
 		var item : Item = nivel.items[posicionNivel];
-		//textItem = new FlxText();
 		textItem = new FlxText(80, 40, 480, null, 26);
 		add(textItem);
+		textItem.text = quitarPuntosItem(item);
 		
-				
-		function mostrarPopUp() {
-			var popup = new FlxUIPopup();
-			var nombresBotones = new Array();
-			nombresBotones.insert(0, "Totalmente mal");
-			nombresBotones.insert(1, "Mal");
-			nombresBotones.insert(2, "Apenas mal");
-			
-			popup.setPosition(0, 0);
-			popup.quickSetup("Respuesta incorrecta", "Indicar que tan mal lo hizo", nombresBotones);
-			popup.create();
-			add(popup);
-			
-		}
-				
-		var btnIncorrecto = new FlxUIButton(560, 280 , "INCORRECTO", mostrarPopUp);
+		var btnIncorrecto = new FlxUIButton(560, 280 , "INCORRECTO", itemIncorrecto);
 		add(btnIncorrecto); 
 				
 		function cambiarPorMayusculas() {
-		textItem.systemFont = "Courrier New";
+			textItem.systemFont = "Calibri";
 			textItem.text = textItem.text.toUpperCase();
 		}
 		
 		function cambiarPorMinusculas() {
-		textItem.systemFont = "Courrier New";
-			textItem.text = textItem.text.toLowerCase();
+			textItem.systemFont = "Calibri";
+			textItem.text =	textItem.text.toLowerCase();
+			textItem.text = textItem.text.charAt(0).toUpperCase() + textItem.text.substring(1,textItem.text.length);
 		}
 		
 		var btnMayusculas = new FlxUIButton(0, 220 , "MAYUSCULAS", cambiarPorMayusculas);
@@ -79,38 +73,27 @@ class Logica extends FlxState
 		var btnMinusculas = new FlxUIButton(0, 250 , "MINUSCULAS", cambiarPorMinusculas);
 		add(btnMinusculas);		
 		
+		btnComenzar = new FlxUIButton(560, 220 , "COMENZAR", comenzar);
+		add(btnComenzar);
 		
-		
-		var btnCorrecto = new FlxUIButton(560, 220 , "CORRECTO", siguienteItem);
-		add(btnCorrecto); 
+		btnCorrecto = new FlxUIButton(560, 220 , "CORRECTO", itemCorrecto);
+		btnCorrecto.visible = false;
+		add(btnCorrecto);
 		
 		function cambiarPorCursiva() {
-			textItem.systemFont = "Arial";
+			textItem.text =	textItem.text.toLowerCase();
+			textItem.text = textItem.text.charAt(0).toUpperCase() + textItem.text.substring(1,textItem.text.length);
+			textItem.systemFont = "Lucida Handwriting Cursiva";
 		}
 		
 		var btnCursiva = new FlxUIButton(0, 280 , "CURSIVA", cambiarPorCursiva);
 		add(btnCursiva);
 		
-		//var btnItemAnterior = new FlxUIButton(200, 220 , "ANTERIOR");
-		//add(btnItemAnterior);
-		//
-		//var btnItemSiguiente = new FlxUIButton(360, 220 , "SIGUIENTE");
-		//add(btnItemSiguiente);
-		//
-		//var btnNivelAnterior = new FlxUIButton(200, 280 , "NIVEL ANTERIOR");
-		//add(btnNivelAnterior);
-		//
-		//var btnNivelSiguiente = new FlxUIButton(360, 280 , "NIVEL SIGUIENTE");
-		//add(btnNivelSiguiente);
-
 		var btnAtras = new FlxUIButton(0, 330 , "ATRAS", irAtras);
 		add(btnAtras);
 		
-
-		
 		textItem.alignment = "center";
-		textItem.text = quitarPuntosItem(item);
-		textItem.systemFont = "Courrier New";
+		textItem.systemFont = "Calibri";
 
 	}
 	
@@ -129,19 +112,15 @@ class Logica extends FlxState
 	function obtenerPartesItem(item:Item):Array<String> {
 		var partes = new Array();
 		var subparte : String;
-		//var bandera : Bool;
-		//bandera = false; por default es false
 		subparte = "";
 		for ( i in 0...item.texto.length) {
 			if (item.texto.substring(i, i + 1) != "." && item.texto.substring(i, i + 1) != " ") {
 				subparte += item.texto.substring(i, i+1);
 				var a : String;
 				a = item.texto.substring(i, i+1);
-				//bandera = true;
 			}
 			else {
 				partes.insert(partes.length, subparte);
-				//partes.add(subparte);
 				subparte = "";
 			}
 		}
@@ -149,59 +128,66 @@ class Logica extends FlxState
 	}
 	
 	function repetirItem() {
-	// si lo hizo mal, repetir el item
+	// SI LO HIZO MAL, REPETIR EL ITEM
 	}
 	
-	function asignarNivelDeMaldad() {
-	//popup con botones. al presionar uno se sale el popup y llama a siguienteItem 
-	}
-	
-	function irAtras()	{
+	function irAtras()	{	
 		FlxG.switchState(new MenuNiveles());
 	}
 
-	function siguienteItem() {
+	function comenzar() {
+		btnComenzar.visible = false;
+		if (posicionNivel < (nivel.items.length)) {
+			//var item : Item = nivel.items[posicionNivel];
+			//color = new FlxTextFormat(FlxColor.AZURE);
+			//
+			//textItem.text = quitarPuntosItem(item);
+			timer = new FlxTimer(0.1, resaltarSilabas, obtenerPartesItem(nivel.items[posicionNivel]).length);
+		}
+	}
+	
+	function itemCorrecto() {
+		btnCorrecto.visible = false;
 		if (posicionNivel < (nivel.items.length - 1)) {
 			posicionNivel += 1;
-			var item : Item = nivel.items[posicionNivel];
-			formatoTween = new FlxTextFormat(FlxColor.GOLDEN);
+			btnComenzar.visible = true;
 			textItem.clearFormats();
-			textItem.text = "";
+			
+			var item : Item = nivel.items[posicionNivel];
+			color = new FlxTextFormat(FlxColor.AZURE);
+			textItem.systemFont = "Calibri";
 			textItem.text = quitarPuntosItem(item);
-			timer = new FlxTimer(0.5, resaltarSilabas, obtenerPartesItem(nivel.items[posicionNivel]).length);
+			
+			//LOGICA DE QUE HACER SI ES CORRECTO
 		}
 	}
 
+	function itemIncorrecto() {
+		//LOGICA DE INCORRECTO (repetir? popup?)
+	}
+	
 	function resaltarSilabas(timer :FlxTimer) {
-		contador = 0;
+		var posicionDentroItem = 0;
 		var silabas = new Array();
-		silabas = obtenerPartesItem(nivel.items[posicionNivel]);
 		var silaba : String;
-		silaba = silabas[timer.elapsedLoops-1];
-
-		formatoTween = new FlxTextFormat(FlxColor.GOLDEN);
-		
+		silabas = obtenerPartesItem(nivel.items[posicionNivel]);
+		silaba = silabas[timer.elapsedLoops - 1];
+		color = new FlxTextFormat(FlxColor.AZURE);
 		for (i in 0...silabas.length) {
-			if (i +1 == timer.elapsedLoops) {
-				if (textItem.text.charAt(contador + silabas[i].length) == " ") {
-					textItem.addFormat(formatoTween, contador, contador + silabas[i].length);
-				}
-				else {
-					textItem.addFormat(formatoTween, contador, contador + silabas[i].length);
-				}
-				break;
+			if (i+1 == timer.elapsedLoops) {
+				textItem.addFormat(color, posicionDentroItem, posicionDentroItem + silabas[i].length);
 			}
 			else {
-				if (textItem.text.charAt(contador + silabas[i].length) == " "){
-					contador += silabas[i].length+1;
+				if (textItem.text.charAt(posicionDentroItem + silabas[i].length) == " "){
+					posicionDentroItem += silabas[i].length+1; 
 				}
 				else {
-					contador += silabas[i].length;
+					posicionDentroItem += silabas[i].length;
 				}
 			}
+		}		
+		if (timer.loopsLeft == 0) {
+			btnCorrecto.visible = true;
 		}
-
-			
-			//textItem.addFormat(formatoTween,
 	}
 }
